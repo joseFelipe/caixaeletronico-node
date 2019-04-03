d49d9fd4c566b552595cd555ca120ca596fe1ed6
Felipe <felipe.florianopolis@gmail.com>
2019-04-03 19:53:56 -0300

    Creating change password method.

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,4 +1,5 @@
 const { User } = require("../models");
+const bcrypt = require("bcrypt");
 
 class UserController {
   create(req, res) {
@@ -23,22 +24,53 @@ class UserController {
   }
 
   async updatePassword(req, res, next) {
-    console.log("* UPDATE PASSWORD *");
     try {
-      const user = User.findById(req.params.id);
-      const { password, password2, password3 } = req.body;
+      const { id } = req.session.user;
+      console.log("ID: " + id);
+      const userModel = new User();
+      let user = await User.findByPk(id);
+      const { password, password1, password2 } = req.body;
+      user = user.toJSON();
+
+      console.log("-- - -- -- -- -- --- --- ");
+      console.log("User: ", user.name);
+      console.log("Password: ", user.password_hash);
+      console.log("-- - -- -- -- -- --- --- ");
+
+      console.log("PASSWORD: " + password);
+      console.log("PASSWORD1: " + password1);
+      console.log("PASSWORD2: " + password2);
+
+      console.log("-- - -- -- -- -- --- --- ");
+
+      const password_hash = await bcrypt.hash(password, 8);
+      const password_hash2 = await bcrypt.hash(password1, 8);
+      const password_hash3 = await bcrypt.hash(password2, 8);
+
+      console.log("- - - - ");
+      console.log("PASSWORD_HASH: " + password_hash);
+      console.log("USER_PASSWORD_HASH: " + user.password_hash);
+      console.log("- - - - ");
+
+      if (bcrypt.compare(password_hash, user.password_hash)) {
+        console.log("Senhas Iguais");
+      } else {
+        console.log("Senhas não iguais");
+      }
+
+      // if (!(await userModel.checkPassword(password))) {
+      //   req.flash("error", "Senha já usada anteriormente");
+      //   return res.redirect("/");
+      // }
+
       // if (password2 != password3) {
       //   req.flash("error", "As novas senhas estão diferentes");
       //   console.log("As novas senhas estão diferentes");
       //   return res.redirect("/app/profile");
       // }
 
-      console.log(req.body.password);
-
       // await user.update(req.body);
-
       // req.flash("success", "Senha alterada com sucesso!");
-
       // res.redirect("/app/dashboard");
     } catch (error) {
       next(error);
